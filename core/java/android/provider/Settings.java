453addf0735aad9521d3ef1fa3eac44f9e41f5f2
Alex Cruz <mazdarider23@gmail.com>
2015-12-24 20:34:11 +0000

    SystemUI: Show 4G instead of LTE switch
    - Added to statusbar items under the AOSP tuner instead of 2 parts
    - Hide the switch on WiFi-Only devices (thanks AICP for the idea)
    - Removed config that was used in overlays
    
    Change-Id: Id0acf0a6b4bd47f5cefba48ffdeb8e546fd439c9
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    @jhenrique09 edits: Code cleanup
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: I95e605c6b6774abcfd5d77a8c59d94b3f5593d88

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4136,6 +4136,15 @@ public final class Settings {
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * Show 4G instead of LTE
+         * @hide
+         */
+        public static final String SHOW_FOURG = "show_fourg";
+
+        /** @hide */
+        public static final Validator SHOW_FOURG_VALIDATOR = BOOLEAN_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4195,6 +4204,7 @@ public final class Settings {
             HAPTIC_FEEDBACK_INTENSITY,
             DISPLAY_COLOR_MODE,
             NOTIFICATION_LIGHT_PULSE,
+            SHOW_FOURG,
         };
 
         /**
@@ -4309,6 +4319,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(EGG_MODE);
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
+            PRIVATE_SETTINGS.add(SHOW_FOURG);
         }
 
         /**
@@ -4400,6 +4411,7 @@ public final class Settings {
             VALIDATORS.put(NOTIFICATION_LIGHT_PULSE, BOOLEAN_VALIDATOR);
             VALIDATORS.put(ACCELEROMETER_ROTATION_ANGLES,
                     ACCELEROMETER_ROTATION_ANGLES_VALIDATOR);
+            VALIDATORS.put(SHOW_FOURG, SHOW_FOURG_VALIDATOR);
         }
 
         /**
